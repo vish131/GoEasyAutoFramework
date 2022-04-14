0a797ac83509a50e440e90c031dd9a286e35c1ff
vishaliAshok <46221037+vishaliAshok@users.noreply.github.com>
2022-04-14 12:21:09 +0530

    First maven project push

---
+++ automation-framework/src/main/java/com/goeasyauto/automation/framework/automation_framework/App.java
@@ -0,0 +1,13 @@
+package com.goeasyauto.automation.framework.automation_framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
