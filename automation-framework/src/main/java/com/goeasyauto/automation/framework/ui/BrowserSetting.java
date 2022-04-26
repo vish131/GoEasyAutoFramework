8a208f8cc44e315fd66b55c34d6fbb196106eddc
Vishnu <imvish131@gmail.com>
2022-04-26 19:02:12 +0530

    Changes in BROwserSETtings

--- automation-framework/src/main/java/com/goeasyauto/automation/framework/ui/BrowserSetting.java
+++ automation-framework/src/main/java/com/goeasyauto/automation/framework/ui/BrowserSetting.java
@@ -69,13 +69,50 @@ public class BrowserSettings {
 		WebElement root7 = shadowRoot6.findElement(By.cssSelector("cr-toggle#cotrol"));
 		
 		elementUtil.click(root7);
+		
+		if(settingType.equalsIgnoreCase("download")) {
+			WebElement root8 = shadowRoot4.findElement(By.cssSelector("category-setting-exceptions"));
+			WebElement shadowRoot8 = expandRootElement(root8);
+			WebElement root9 = shadowRoot8.findElement("site-list[category-header='Allow']");
+			WebElement shadowRoot9 = expandRootElement(root9);
+			WebElement root10 = shadowRoot9.findElement(By.cssSelector("cr-button#addsite"));
+			
+			elementUtil.click(root10);
+			Log.info("clicked Add Site in Download Allow");
+			
+			WebElement root11 = shadowRoot9.findElement(By.cssSelector("add-site-dialog"));
+			WebElement shadowRoot11 = expandRootElement(root11);
+			WebElement root12 = shadowRoot11.findElement(By.cssSelector("cr-input#site"));
+			WebElement shadowRoot12 = expandRootElement(root12);
+			WebElement root13 = shadowRoot12.findElement(By.cssSelector("input#input"));
+			
+			elementUtil.enterText(root13, applicationUrl);
+			Log.info("Entered {} in Download Allow TextBox", applicationUrl);
+			
+			WebElement root14 = shadowRoot11.findElement(By.cssSelector("cr-button#add"));
+			
+			elementUtil.click(root14);
+			Log.info("CLicked Add Button");
+			
+		}
+		
+		return result;
+				
 	}
 	
 	
+	/*
+	 * To expand the shadow root element in chrome setting page
+	 * 
+	 * Element - Webelement to expand
+	 * 
+	 * return by locator of element
+	 * */
 	
-	
-	
-	
-	
+	private WebElement expapndRootElement(WebElement element) {
+		WebElement ele = (WebElement) elementUtil.jsExecuteScriptWithReturn("return arguments[0].shadowRoot",element);
+		Log.info("Element: {}", ele);
+		return ele;
+	}	
 	
 }
