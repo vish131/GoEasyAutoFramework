88f6e737c6123ad0aa266a1ba504c9c2d56dfae7
vishn <vishn@Vishnu>
2024-03-29 20:18:12 +0800

    changes

--- automation-framework/src/main/java/com/goeasyauto/automation/framework/automation_framework/App.java
+++ automation-framework/src/main/java/com/goeasyauto/automation/framework/automation_framework/App.java
@@ -11,6 +11,8 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! 2" );
         System.out.println( "Done" );
+        System.out.println( "Done 2" );
+        
         
     }
 }
